e5bf470ec24d1a130207253a156df43d9c6d49c7
damir-dev-21 <devdamiricher@gmail.com>
2023-09-04 15:09:53 +0500

    init

---
+++ android/app/src/main/kotlin/com/example/code_union_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.code_union_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/marks_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MarksScreen extends StatefulWidget {
+  MarksScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MarksScreen> createState() => _MarksScreenState();
+}
+
+class _MarksScreenState extends State<MarksScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
